d4b5258e7fa58260ecd6dc976753cc5780815e07
Paulo Mesquita <paulomesquita0@gmail.com>
2025-09-09 09:35:53 -0300

    1

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,15 @@
 import "./Login.css";
 import Button from "../components/Button";
+import { IMaskInput } from "react-imask";
+import { useState } from "react";
 
 interface LoginProps {
   onLogin: () => void;
 }
+
 export default function Login({ onLogin }: LoginProps) {
+  const [cpfOrDate, setCpfOrDate] = useState("");
+
   return (
     <div className="login-page">
       <div className="login-teste">
@@ -12,18 +17,36 @@ export default function Login({ onLogin }: LoginProps) {
           <h1>Portal do Beneficiário</h1>
         </div>
         <div className="login-card">
-          <div className="header ">
+          <div className="header">
             <h2>Login</h2>
             <small>
               Entre com seus dados corretamente para acessar nossa plataforma.
             </small>
           </div>
           <div className="body-card_login">
-            <label>CPF</label>
-            <input
+            <label>Data de Nascimento / CPF</label>
+            <IMaskInput
               style={{ marginBottom: "1.5rem" }}
-              type="text"
-              placeholder="000.000.000-00"
+              className="login-input"
+              mask={[
+                { mask: "00/00/0000" }, // Data
+                { mask: "000.000.000-00" }, // CPF
+              ]}
+              value={cpfOrDate}
+              onAccept={(value: string) => setCpfOrDate(value)}
+              placeholder="Data de nascimento ou CPF"
+              dispatch={(appended, dynamicMasked) => {
+                const value = (dynamicMasked.value + appended).replace(
+                  /\D/g,
+                  ""
+                );
+                // até 8 dígitos → Data
+                if (value.length <= 8) {
+                  return dynamicMasked.compiledMasks[0];
+                }
+                // mais que 8 → CPF
+                return dynamicMasked.compiledMasks[1];
+              }}
             />
 
             <label>Senha</label>
